ae65152902f33400a9e9b1395e9fbdb8447be58c
Tomass Wilson <thmwi@kth.se>
2023-08-29 18:30:38 +0200

    add bidding to players

--- src/dogpark/game/player.py
+++ src/dogpark/game/player.py
@@ -7,6 +7,7 @@ class Player:
 
     def __init__(self, colour: str, is_human: bool = True, is_physical: bool = False):
         self.is_human = is_human  # TODO: implement AI
+        self.ai = None  # TODO: implement AI
         self.is_physical = is_physical
         self.colour = colour
         self.kennel = []
@@ -36,6 +37,41 @@ class Player:
             self.objective = easy  # TODO: implement AI
             if self.is_physical:
                 print(f"{self.colour} chose {self.objective}")
+
+    def bid(self, available_dogs: dict[str, dict]) -> (str, int):
+        """for a given dict of dogs, choose one and return its name and bid amount"""
+        if self.is_human:
+            if self.is_physical:
+                print(f"Player {self.colour}, which dog did you bid on?")
+                dog = input()
+                # TODO: consider that this may be hidden from the program if playing against physical players
+                bid = int(input("How much did you bid? "))
+            else:
+                print(f"Player {self.colour}, Choose a dog:")
+                print(available_dogs)
+                dog = input()
+                bid = int(input("How much would you like to bid? "))
+            return dog, bid
+
+        else:
+            dog = list(available_dogs.keys())[0]  # TODO: implement AI
+            print(f"{self.colour} chose {dog}")
+            return dog, 1
+
+    def choose_dog(self, available_dogs: dict[str, dict]) -> str:
+        """for a given dict of dogs, choose one and return its name, for a cost of 1 rep"""
+        if self.is_human:
+            if self.is_physical:
+                print(f"Player {self.colour}, which dog did you choose?")
+                dog = input()
+            else:
+                print(f"Player {self.colour}, Choose a dog:")
+                print(available_dogs)
+                dog = input()
+        else:
+            dog = list(available_dogs.keys())[0]  # TODO: implement AI
+            print(f"{self.colour} chose {dog}")
+        return dog
 
     def __repr__(self):
         return f"{self.colour} - {self.reputation} REP - {self.kennel} - {self.resources} - {self.objective}"
