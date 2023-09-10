add6ade6a0b8e80de4048d90571337c20130d2d5
Tomass Wilson <thmwi@kth.se>
2023-09-10 14:19:56 +0200

    add helper to reload dogs

--- src/dogpark/game/dog.py
+++ src/dogpark/game/dog.py
@@ -4,3 +4,9 @@ import yaml
 
 with open(importlib.resources.files("dogpark.game") / "dogs.yaml") as f:
     DOGS = yaml.safe_load(f)
+
+
+def reload_dogs() -> dict[str, dict]:
+    with open(importlib.resources.files("dogpark.game") / "dogs.yaml") as f:
+        dogs = yaml.safe_load(f)
+    return dogs
